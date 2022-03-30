ed3cdcbef6c8f2b0efbf0ba677611b1f245b44b7
Jason Alvarez <JasonRoyAlvarez@gmail.com>
2022-03-30 13:08:24 -0700

    commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7zwe_8um+#l5_l--5o&pu3p&kp&4cod2z#m77^jzq^f(dy_@+f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
